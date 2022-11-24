aed783a1549cb94377924c434e1435196c21a235
Trent1048 <trentbultsma@gmail.com>
2022-11-23 21:34:19 -0800

    add image opening button functionality

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+from tkinter import filedialog
 
 class SteganographyUI(tk.Tk):
     """The GUI for the image steganography program."""
@@ -30,14 +31,14 @@ class SteganographyUI(tk.Tk):
         text_box.grid(row=0, column=1)
 
         # setup image display
-        self.image_holder = tk.Label(self, text="No File Loaded")
+        self.image_holder = tk.Label(self, text="No File Loaded", foreground="red")
         self.image_holder.grid(row=0, column=0)
 
         # setup load and export and read buttons
         self.button_frame = tk.Frame(self)
         self.button_frame.grid(row=1, column=0)
         self.load_image_button = tk.Button(self.button_frame, 
-            text="Load Image", command=self.placeholder)
+            text="Load Image", command=self.set_image)
         self.load_image_button.grid(row=0, column=0)
         self.export_message_button = tk.Button(self.button_frame, 
             text="Export Message", command=self.placeholder)
@@ -46,17 +47,20 @@ class SteganographyUI(tk.Tk):
             text="Read Message", command=self.placeholder)
         self.read_message_button.grid(row=0, column=2)
 
-        self.set_image("test.png")
-
     def placeholder(self):
         pass
 
-    def set_image(self, image_path):
+    def set_image(self):
         """Sets the image for the selected image display box."""
-        image_source = Image.open(image_path)
-        image_source.thumbnail((200, 200))
-        self.image = ImageTk.PhotoImage(image_source)
-        self.image_holder.configure(image=self.image)
+        image_path = filedialog.askopenfilename()
+        if image_path.lower().endswith(".png"):
+            image_source = Image.open(image_path)
+            image_source.thumbnail((200, 200))
+            self.image = ImageTk.PhotoImage(image_source)
+            self.image_holder.configure(image=self.image)
+            self.selected_image_path = image_path
+        else:
+            self.image_holder.configure(image="", text="Must select a .png image")
 
 if __name__ == "__main__":
     app = SteganographyUI()
